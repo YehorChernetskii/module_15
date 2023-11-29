ce837682a4ab33a320557db064916d41091d5fdc
Yehor Chernetskii <egorchernetskii@gmail.com>
2023-11-29 13:43:52 +0200

    name_list_added

--- 01_list_gen/main.py
+++ 01_list_gen/main.py
@@ -1,1 +1,13 @@
 # TODO здесь писать код
+
+def generate_odd_numbers(N):
+    odd_numbers = []
+    for i in range(1, N + 1, 2):
+        odd_numbers.append(i)
+    return odd_numbers
+
+N = int(input("Введите целое число N: "))
+
+result_list = generate_odd_numbers(N)
+
+print("Список нечетних чисел от 1 до", N, ":", result_list)
--- 02_tournament/main.py
+++ 02_tournament/main.py
@@ -1,1 +1,6 @@
 # TODO здесь писать код
+
+participants = ["Артемий", "Борис", "Влад", "Гоша", "Дима", "Евгений", "Женя", "Захар"]
+
+for names in range(0, len(participants), 2):
+    print(participants[names])
