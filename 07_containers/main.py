0319047dd99a181aab2c3769a73052611e8128af
Yehor Chernetskii <egorchernetskii@gmail.com>
2023-11-29 21:00:44 +0200

    container_sorting_added

--- 07_containers/main.py
+++ 07_containers/main.py
@@ -1,1 +1,17 @@
 # TODO здесь писать код
+
+n = int(input("Кол-во контейнеров: "))
+
+weights = []
+
+for _ in range(n):
+    weight = int(input("Введите вес контейнера: "))
+    weights.append(weight)
+
+new_weight = int(input("Введите вес нового контейнера: "))
+
+position = 1
+while position <= n and weights[position - 1] >= new_weight:
+    position += 1
+
+print("Номер, куда встанет новый контейнер:", position)
