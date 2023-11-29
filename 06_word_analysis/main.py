35cd321aae4e7940592d79c8d551706ed94208de
Yehor Chernetskii <egorchernetskii@gmail.com>
2023-11-29 19:39:23 +0200

    unique_letters_added

--- 06_word_analysis/main.py
+++ 06_word_analysis/main.py
@@ -1,1 +1,9 @@
 # TODO здесь писать код
+
+word = input("Введите слово: ")
+
+unique_letters = set(word)
+
+count_unique_letters = len(unique_letters)
+
+print(f"Кол-во уникальных букв: {count_unique_letters}")
