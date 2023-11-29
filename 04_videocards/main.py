b0caed4804faef99423ca9d1f9a69fc1b7f79747
Yehor Chernetskii <egorchernetskii@gmail.com>
2023-11-29 15:18:41 +0200

    generation_select_added

--- 04_videocards/main.py
+++ 04_videocards/main.py
@@ -1,3 +1,17 @@
 # TODO здесь писать код
 
+n = int(input("Кол-во видеокарт: "))
 
+graphics_cards = []
+
+for i in range(1, n + 1):
+    model = int(input(f"{i} Видеокарта: "))
+    graphics_cards.append(model)
+
+print("Старый список видеокарт:", graphics_cards)
+
+max_value = max(graphics_cards)
+
+graphics_cards = [value for value in graphics_cards if value != max_value]
+
+print("Новый список видеокарт:", graphics_cards)
