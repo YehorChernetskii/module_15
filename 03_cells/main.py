89fd3867d1a901e50fad72e385337dbc66648481
Yehor Chernetskii <egorchernetskii@gmail.com>
2023-11-29 15:04:18 +0200

    cells_select_added

--- 03_cells/main.py
+++ 03_cells/main.py
@@ -1,1 +1,15 @@
 # TODO здесь писать код
+
+n = int(input("Кол-во клеток: "))
+
+efficiency_values = []
+
+for i in range(1, n + 1):
+    efficiency = int(input(f"Эффективность {i} клетки: "))
+    efficiency_values.append(efficiency)
+
+print("Неподходящие значения:", end=' ')
+for i in range(n):
+    if efficiency_values[i] < i + 1:
+        print(efficiency_values[i], end=' ')
+print()
