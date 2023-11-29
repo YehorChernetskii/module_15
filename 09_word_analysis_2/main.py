cf6b59b32be9adf89072db04c0e339468fbac30b
Yehor Chernetskii <egorchernetskii@gmail.com>
2023-11-29 21:44:15 +0200

    palindrome_check_added

--- 09_word_analysis_2/main.py
+++ 09_word_analysis_2/main.py
@@ -1,1 +1,12 @@
 # TODO здесь писать код
+
+def is_palindrome(word):
+    reversed_word = word[::-1]
+    return word == reversed_word
+
+input_word = input("Введите слово: ")
+
+if is_palindrome(input_word):
+    print("Слово является палиндромом")
+else:
+    print("Слово не является палиндромом")
