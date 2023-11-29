924a4311a9701c976033b5b4e0b4c9eebdf5a9f5
Yehor Chernetskii <egorchernetskii@gmail.com>
2023-11-29 22:11:55 +0200

    selection_sort_added

--- 10_sort/main.py
+++ 10_sort/main.py
@@ -1,1 +1,13 @@
 # TODO здесь писать код
+
+def selection_sort(my_list):
+    for i_mn in range(len(my_list)):
+        for curr in range(i_mn + 1, len(my_list)):
+            if my_list[curr] < my_list[i_mn]:
+                my_list[curr], my_list[i_mn] = my_list[i_mn], my_list[curr]
+
+nums = [1, 4, -3, 0, 10]
+
+selection_sort(nums)
+
+print(nums)
